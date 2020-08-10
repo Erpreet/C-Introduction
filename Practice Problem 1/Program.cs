6b555241bd8f198a7d66e4870ea83419a0c0ea3e
Erpreet <preetkaurchd15@gmail.com>
2020-08-09 20:54:31 -0600

    Sum of 2 numbers - practice problem 1

---
+++ Practice Problem 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace practice_problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number");
+            string input1 = Console.ReadLine();
+            double number1 = Convert.ToDouble(input1);
+
+            Console.Write("Enter second number");
+            string input2 = Console.ReadLine();
+            double number2 = Convert.ToDouble(input2);
+
+            double sum = number1 + number2;
+
+            Console.WriteLine("Sum is {0} ",sum);
+
+            Console.ReadLine();
+        }
+    }
+}
