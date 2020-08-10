afe498172e6f54c2d77b5d6d5e756a5006759caa
Erpreet <preetkaurchd15@gmail.com>
2020-08-09 23:56:33 -0600

    cube of a given number

---
+++ Cubeofnumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cubeofnumber
+{
+    class Program
+    {
+         static void Main()
+        {
+           int myNum, cube;
+           myNum = 15;
+           cube = myNum*myNum*myNum;
+           Console.WriteLine(cube);
+        }
+    }
+}
